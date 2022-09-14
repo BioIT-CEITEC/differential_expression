b3fc423959f4a137aa7faab24b4ce3a390d51948
vojtab84 <vojtab84@gmail.com>
2022-09-14 10:52:51 +0200

    refactoring DE beta version 5.1

--- wrappers/DE_computation/DESeq2_edgeR_overlap_func.R
+++ wrappers/DE_computation/DESeq2_edgeR_overlap_func.R
@@ -1,8 +1,12 @@
 comparison_specific_edgeR_DESeq2_overlap <- function(output_dir,comp_res,edgeR_comp_res,p_value_threshold,lfc_threshold){
+  orig_dir <- getwd()
+  dir.create(paste0(output_dir,"/report_data"),showWarnings = F,recursive = T)
+  setwd(output_dir)
+
   ####################################################################################################
   # Overlap between DESeq2 and edgeR and Venn diagrams
   # http://www.ats.ucla.edu/stat/r/faq/venn.htm
-  
+
   vennTable <- merge(comp_res[,.(Feature_name,DESeq2 = significant_DE * sign(log2FoldChange),DESeq2_padj = padj,DESeq2_log2FoldChange = log2FoldChange)],
         edgeR_comp_res[,.(Feature_name,edgeR = significant_DE * sign(log2FoldChange),edgeR_padj = padj,edgeR_log2FoldChange = log2FoldChange)],by = "Feature_name")
   
@@ -18,9 +22,9 @@ comparison_specific_edgeR_DESeq2_overlap <- function(output_dir,comp_res,edgeR_c
   vennTable[,DESeq2_mean_padj := (DESeq2_padj + edgeR_padj) / 2]
   setorder(vennTable,DESeq2_mean_padj,na.last = T)
   setcolorder(vennTable,c(1,2,5,8,3,6,4,7))
-  
-  
+
   fwrite(vennTable, file = "overlap_DESeq2_edgeR_de.tsv", sep="\t")
 
+  setwd(orig_dir)
 }
 
