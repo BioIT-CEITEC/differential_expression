8fd15d95fbe79c8a187e1b8a72fe9562462503fb
Nicolas <nicolas.blavet@ceitec.muni.cz>
2022-11-16 09:56:47 +0100

    adding scripts for salmon and kallisto

---
+++ wrappers/analysis_kallisto_table/script.py
@@ -0,0 +1,21 @@
+#############################################################
+# wrapper for rule: analysis_kallisto_table
+#############################################################
+import os
+from snakemake.shell import shell
+shell.executable("/bin/bash")
+log_filename = str(snakemake.log)
+
+
+f = open(log_filename, 'a+')
+f.write("\n##\n## RULE: analysis_kallisto_table \n##\n")
+f.close()
+
+command = "$CONDA_PREFIX/bin/Rscript "+os.path.abspath(os.path.dirname(__file__))+"/combine_kallisto_tabs.R "+\
+            snakemake.output.kallisto + " " +\
+            snakemake.params.tx2gene + " " +\
+            " ".join(snakemake.input.kallisto) + " >> " + log_filename + " 2>&1 "
+
+f = open(log_filename, 'a+')
+f.write("## COMMAND: "+command+"\n")
+shell(command)
